12e624fd4c94a5032857be285681476cf28e9a5a
James <Jameshoggard.dev@gmail.com>
2021-12-04 22:10:58 -0500

    updB

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function App() {
+export default function App() {
+
+    const url = 'https://passwordinator.herokuapp.com/generate'
+
+    const [passW, setPassW] = useState([{}]);
+
+    const fetchURes = () => {
+        fetch(url).then(
+            resp => resp.json()
+        ).then(
+            data => {
+                setPassW(data);
+            }
+        )
+    };
+
     return (
         <div>
-             <button  type="submit" className="p-2 pl-5 pr-5 bg-transparent border-2 border-indigo-500 text-indigo-500 text-lg rounded-lg transition-colors duration-700 transform hover:bg-green-800 hover:text-gray-100 focus:border-4 focus:border-indigo-300">Generate</button>
+             <button onClick={fetchURes} type="submit" className="p-2 pl-5 pr-5 bg-transparent border-2 border-indigo-500 text-indigo-500 text-lg rounded-lg transition-colors duration-700 transform hover:bg-green-800 hover:text-gray-100 focus:border-4 focus:border-indigo-300">Generate</button>
         </div>
     )
-}
-
-export default App
+};
